92ed5fda8ccafcbfa7ceba61a73a77086b7453cd
MarkFGit <mfenner12345@gmail.com>
2021-05-30 13:51:47 -0400

    sloppy code push

--- backend.py
+++ backend.py
@@ -1,19 +1,45 @@
 import os
 from flask import Flask, redirect, url_for, render_template, send_from_directory, send_file
-#import re
-from ctypes import wintypes, windll
-from functools import cmp_to_key
+from tinytag import TinyTag
+from PIL import Image
+from io import BytesIO
+from base64 import b64encode
 
-# template_dir = os.path.abspath
 app = Flask(__name__, template_folder='html', static_folder='static')
 
 @app.route("/")
 def home():
-	path = 'C:/Users/markr/Desktop/Website/static/media/songs'
+	path = 'C:/Users/markr/Desktop/Website/static/media/songs/'
 	songNames = sorted(os.listdir(path), key=len)
-	#data = os.listdir(path)
 	numSongs = len(os.listdir(path))
+
+
+	#start loop
+	for song in songNames:
+		coverImg = TinyTag.get(path+song, image = True)
+		coverImage_data = coverImg.get_image()
+
+		if (coverImage_data != None): #Modify this ***********
+			coverBytes = Image.open(BytesIO(coverImage_data))
+			print(song)
+			print(song[:-4])
+			print("\n\n\n")
+			coverBytes.save('C:/Users/markr/Desktop/Website/static/media/songCovers/'+song[:-4]+".jpeg")
+
+	#output = BytesIO()
+	#coverBytes.save(output, format='JPEG')
+	#image_data = b64encode(output.getvalue())
+
+
+	# if (type(image_data) != str):
+	# 	image_data = image_data.decode() #Turns bytes to string
+
+	# data_url = 'data:image/jpeg;base64,' + image_data
+	#end loop
+
 	return render_template('index.html', songNames = songNames, numSongs = numSongs)
+
+
 
 @app.route("/Playlists")
 def playlists():
@@ -25,6 +51,27 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
+def checkImages():
+	return foo
+
+
 if __name__ == "__main__":
+	path = 'C:/Users/markr/Desktop/Website/static/media/songs'
+	song = TinyTag.get(path+"/6.mp3")
+
+	print("Title: " + str(song.title))
+	print("Duration: " + str(song.artist))
+	print("Duration: " + str(song.duration))
+	print("Duration: " + str(song.duration))
+
+
+
 	app.run(debug=True)
 	
+
+
+#May Want Class for object that can hold:
+	#Song Title
+	#Song Artist
+	#Song Image Data (if present)
+	#
--- static/script.js
+++ static/script.js
@@ -1,5 +1,23 @@
 var lastSong = null;
 var table = document.getElementById("songTable");
+var mainAudio = document.getElementById("mainAudio");
+
+mainAudio.addEventListener('timeupdate', function(){
+	position = mainAudio.currentTime / mainAudio.duration;
+	fill.style.width = position *  100 + '%';
+	handlef.style.marginLeft = (position * 100) - 1.8 + '%';
+
+	if(position == 1){ //if the song finishes
+		let lastSongObject = table.rows[lastSong-1].cells[0].firstElementChild.parent;
+		lastSongObject.newImg.src = 'static/media/play.png';
+		position = 0;
+		fill.style.width = 0 + '%';
+		handlef.style.marginLeft = 0 + '%';
+		//revert the song that just finished back to its original state
+		//then move to the next song (if there is a next song)
+		table.rows[lastSong].cells[0].firstElementChild.click();
+	}
+});
 
 document.getElementById("seekBar").onmousedown = mouseDown;
 
@@ -11,9 +29,11 @@ function mouseDown(e) {
 		handlef.style.marginLeft = (e.clientX / window.innerWidth)*(100 * (2-.96)) - 3.2 + "%";
 		fill.style.width = (e.clientX / window.innerWidth)*(100 * (2-.95))- 1.6  + "%";
 		table.rows[lastSong-1].cells[0].firstElementChild.parent.song.currentTime = (parseFloat(fill.style.width)/100) * table.rows[lastSong-1].cells[0].firstElementChild.parent.song.duration;
-		table.rows[lastSong-1].cells[0].firstElementChild.parent.song.pause();
+		
 	}
-	
+	//if(table.rows[lastSong-1].cells[0].firstElementChild.src == "static/media/pause.png"){
+			table.rows[lastSong-1].cells[0].firstElementChild.parent.song.pause();
+		//}
 	document.onmouseup = stopDragElement;
 	document.onmousemove = dragElement;
 }
@@ -32,7 +52,9 @@ function dragElement(e) {
 function stopDragElement() {
 	document.onmouseup = null;
 	document.onmousemove = null;
-	table.rows[lastSong-1].cells[0].firstElementChild.parent.song.play();
+	if(table.rows[lastSong-1].cells[0].firstElementChild.src == "http://127.0.0.1:5000/static/media/pause.png"){ //if it was playing (paused temporarily due to dragElement), then play
+		table.rows[lastSong-1].cells[0].firstElementChild.parent.song.play();
+	} 
 }
 
 
@@ -55,49 +77,68 @@ function addRow(songCount){
 	let td2 = tr.insertCell(1);
 	let td3 = tr.insertCell(2);
 
-	var songTest = new addSongObject(songCount);
-	
-	songTest.newImg.addEventListener('click', function(){
-		if(lastSong != null && lastSong != songTest.songNum){ //If a song has been played before, and the last song is different than the current song
-			let lastAudio = table.rows[lastSong-1].cells[0].firstElementChild.parent.song;
+	var songObject = new addSongObject(songCount);
 
-			//preparation to remove audio element
-			lastAudio.pause();
-			var lastAudioSrc = lastAudio.src;
-			lastAudio.src = "";
-			lastAudio.load();
-			lastAudio.remove();
-			//need to remove the audio element in order to free up a socket
+	songObject.newImg.addEventListener('click', function(){
+		if(lastSong != songObject.songNum){
+			if(lastSong != null){ //If a song has been played before, and the last song is different than the current song
+				//let lastAudio = table.rows[lastSong-1].cells[0].firstElementChild.parent.song;
 
-			//recreate audio element
-			lastAudio.song = document.createElement('audio');
-			lastAudio.setAttribute('id','song');
-			lastAudio.setAttribute('type','audio/mp3');
-			lastAudio.setAttribute('src', lastAudioSrc);
+				//preparation to remove audio element
+				//lastAudio.pause();
+				//var lastAudioSrc = lastAudio.src;
+				/*lastAudio.src = "";
+				lastAudio.load();
+				lastAudio.remove();
+				//need to remove the audio element in order to free up a socket
 
-			//set corresponding image to the paused position
-			table.rows[lastSong-1].cells[0].firstElementChild.src = 'static/media/play.png';
+				//recreate audio element
+				lastAudio.song = document.createElement('audio');
+				lastAudio.setAttribute('id','song');
+				lastAudio.setAttribute('type','audio/mp3');
+				lastAudio.setAttribute('src', lastAudioSrc);*/
+
+				//set corresponding image to the paused icon
+				table.rows[lastSong-1].cells[0].firstElementChild.src = 'static/media/play.png';
+				songObject.isPlaying = false; //??????????
+			}
+			mainAudio.src = 'static/media/songs/' + songObject.newSpan.innerText + ".mp3";
 		}
 
 
-		if(songTest.newImg.src == "http://127.0.0.1:5000/static/media/play.png"){
-			songTest.song.play();
-			songTest.newImg.src = 'static/media/pause.png';
+		if(songObject.isPlaying == false){//songObject.newImg.src == "http://127.0.0.1:5000/static/media/play.png"){
+			console.log(table.rows[lastSong-1]?.cells[0].firstElementChild.parent.song);
+			
+			mainAudio.play();
+			songObject.newImg.src = 'static/media/pause.png';
+			songObject.isPlaying = true;
 		}
 		else{
-			songTest.song.pause();
-			songTest.newImg.src = 'static/media/play.png';
+			var playPromise = mainAudio.play();
+
+			if(playPromise !== undefined){
+				playPromise.then(_ => {
+					mainAudio.pause();
+					songObject.newImg.src = 'static/media/play.png';
+				})
+				.catch(error => {
+
+				})
+			}
+			songObject.isPlaying = false;
+			//songObject.song.pause();
+			//songObject.newImg.src = 'static/media/play.png';
 		}
 		
-		lastSong = songTest.songNum;
+		lastSong = songObject.songNum;
 	});
-	songTest.song.addEventListener('timeupdate', function(){
-		position = songTest.song.currentTime / songTest.song.duration;
+	/*songObject.song.addEventListener('timeupdate', function(){
+		position = songObject.song.currentTime / songObject.song.duration;
 		fill.style.width = position *  100 + '%';
 		handlef.style.marginLeft = (position * 100) - 1.8 + '%';
 
 		if(position == 1){ //if the song finishes
-			songTest.newImg.src = 'static/media/play.png';
+			songObject.newImg.src = 'static/media/play.png';
 			position = 0;
 			fill.style.width = 0 + '%';
 			handlef.style.marginLeft = 0 + '%';
@@ -105,55 +146,67 @@ function addRow(songCount){
 			//then move to the next song (if there is a next song)
 			table.rows[lastSong].cells[0].firstElementChild.click();
 		}
-	});
+	});*/
 
 
-	td.appendChild(songTest.newImg);
-	td2.appendChild(songTest.newSpan);
+	td.appendChild(songObject.newImg);
+	td2.appendChild(songObject.newSpan);
 }
 
 function addSongObject(songCount){
 	this.newSpan = document.createElement('span');
 	this.newSpan.style.color = "grey";
-	//var myData = ['{{ data }}'];
-	//this.newSpan.appendChild(document.createTextNode('media/'));//+songCount+'.mp3'));
 	
 	this.newImg = document.createElement('img');
 	this.newImg.setAttribute('id','newImg');
 	this.newImg.setAttribute('type','img');
+	//var image_data = document.getElementById('scriptTag').getAttribute('image_data')
+	//if(image_data != 'None') table.rows[0].cells[0].firstElementChild.setAttribute('src', image_data);
 	this.newImg.setAttribute('src','static/media/play.png');
 
-	this.newImg.parent = this;
+	this.newImg.parent = this; //way to reference the object itself
 
-	this.song = document.createElement('audio');
-	this.song.setAttribute('id','song');
-	this.song.setAttribute('type','audio/mp3');
-	//this.song.setAttribute('src', 'static/media/songs/'+songCount.toString()+'.mp3');
+	//this.song = document.createElement('audio');
+	//this.song.setAttribute('id','song');
+	//this.song.setAttribute('type','audio/mp3');
 
-	this.song.volume = 1;
+	//this.song.volume = 1;
 
 	this.position;
 
 	this.songNum = songCount;
+
+	this.isPlaying = false;
+
+
 }
 
 function addEntryInfo(){
-	var x = document.getElementById('scriptTag').getAttribute('songNames');
-	var arr = x.split(",");
+	//var x = document.getElementById('scriptTag').getAttribute('songNames');
+	//var arr = x.split(",");
 
-	arr[0] = arr[0].slice(2,arr[0].length-1); //handle first element with opening bracket
-	for(var count = 1; count < arr.length-1; count++){
+	songList = arrayify('songNames');
+	//arr[0] = arr[0].slice(2,arr[0].length-1); //handle first element with opening bracket
+	
+	for(var count = 0; count < songList.length; count++){ 
+		table.rows[count].cells[0].firstElementChild.parent.newSpan.innerText = songList[count].slice(0, songList[count].length-4);//appendChild(document.createTextNode(songList[count].slice(0, songList[count].length-4))); 
+		//table.rows[count].cells[0].firstElementChild.parent.song.setAttribute('src','static/media/songs/'+songList[count]);
+		if(count == 4) table.rows[4].cells[0].firstElementChild.setAttribute('src', "http://127.0.0.1:5000/static/media/songCovers/" + songList[count].slice(0, songList[count].length-4) + ".jpeg");
+	}
+	//var image_data = document.getElementById('scriptTag').getAttribute('image_data')
+	//if(image_data != 'None') table.rows[4].cells[0].firstElementChild.setAttribute('src', "http://127.0.0.1:5000/static/media/songCovers/" + arr[count]);
+}
+
+function arrayify(getAttribute){
+	var arr = document.getElementById('scriptTag').getAttribute(getAttribute);
+	arr = arr.split(",");
+
+	for(var count = 0; count < arr.length; count++){
 		arr[count] = arr[count].slice(2,arr[count].length - 1);
 	}
-	arr[arr.length-1] = arr[arr.length-1].slice(2,arr[arr.length-1].length-2); //handle last element with closing bracket
+	arr[arr.length-1] = arr[arr.length-1].slice(0, arr[arr.length-1].length-1); //handle last element with closing bracket
 
-	//table.rows[1].cells[0].firstElementChild.parent.newSpan.appendChild(document.createTextNode(arr[1]));
-	for(var count = 0; count < arr.length; count++){ 
-		table.rows[count].cells[0].firstElementChild.parent.newSpan.appendChild(document.createTextNode(arr[count])); 
-		table.rows[count].cells[0].firstElementChild.parent.song.setAttribute('src','static/media/songs/'+arr[count]);
-
-	}
-
+	return arr;
 }
 
 document.getElementById("playPrev").addEventListener('click',function(){
@@ -173,3 +226,6 @@ document.getElementById("playNext").addEventListener('click',function(){
 	//multiple pages to select from, ex: home, playlists, artists
 	//volume slider
 	//figure out how to interact with file system; get info from the song files themselves (artist, song name, song picture(if present))
+
+
+//Add windows volume slider thumbnail?
