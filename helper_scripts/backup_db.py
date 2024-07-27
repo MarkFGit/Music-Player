0482a6b651c420012d7360b9e0f24efe3fb43db6
Mark Fenner <mfenner12345@gmail.com>
2024-07-27 13:53:34 -0400

    Automatic local database backups now work on GNU/Linux systems.

--- helper_scripts/backup_db.py
+++ helper_scripts/backup_db.py
@@ -3,10 +3,13 @@ import os
 import subprocess
 import shutil
 import sys
+import platform
 
 # -------- Local Script Imports --------
 import backend_env
 # --------------------------------------
+
+
 
 def back_up_db(db_name: str) -> None:
 	"""
@@ -30,21 +33,36 @@ def back_up_db(db_name: str) -> None:
 	error_file_path = f"{new_dir_path}/ERROR_{date_file_name}.sql"
 
 	backup_db_to_local_dir(db_name, normal_file_path, error_file_path)
-	copy_backup_to_MEGA(normal_file_path, error_file_path, date_file_name)
+	# copy_backup_to_MEGA(normal_file_path, error_file_path, date_file_name)
 
 	print(f"Backed up at {date_file_name}")
 
 
 def backup_db_to_local_dir(db_name: str, normal_file_path: str, error_file_path: str) -> None:
 	""" Runs MySQLDump and saves the results in the location of '/{the_current_website_dir}/local backups/{datetime}/' """
-	subprocess.run(
-		# login via this method. Found this method from https://stackoverflow.com/a/28021494/16432352
-		["mysqldump", "--login-path=local", db_name],
-		shell=True,
-		cwd="C:/Program Files/MySQL/MySQL Server 8.0/bin/",
-		stdout=open(normal_file_path,"w"),
-		stderr=open(error_file_path, "w")
-	)
+	if platform.system() == "Windows":
+		subprocess.run(
+			# login via this method. Found this method from https://stackoverflow.com/a/28021494/16432352
+			["mysqldump", "--login-path=local", db_name],
+			shell=True,
+			cwd="C:/Program Files/MySQL/MySQL Server 8.0/bin/",
+			stdout=open(normal_file_path,"w"),
+			stderr=open(error_file_path, "w")
+		)
+	elif platform.system() == "Linux":		
+		# This variable depends on where the server is started from! Please make sure you are running the server from the base folder to ensure this is working correctly.
+		parent_process_full_cwd = sys.path[0]
+
+		subprocess.run(
+			["mysqldump", f"--defaults-extra-file={parent_process_full_cwd}/dbconfig.cnf"],
+			cwd="/bin/",
+			stdout=open(normal_file_path, "w"),
+			stderr=open(error_file_path, "w")
+		)
+		# Also suppress error password error? (Delete file if password text present)
+	else:
+		print("Failed to make a local backup!")
+		return
 
 	no_error = (os.stat(error_file_path).st_size == 0)
 	if no_error:
